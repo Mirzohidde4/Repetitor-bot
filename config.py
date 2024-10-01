c169a6567db1560614a7dcf75675b39597eff272
Mirzohidde4 <mirzohidxudayberganov27@gmail.com>
2024-10-01 11:02:50 +0500

    *

---
+++ config.py
@@ -0,0 +1,16 @@
+# TOKEN = '6842270549:AAEVq-VSV96HcbKmrHuTWwqIB-d4Q2cbUZQ'
+# ADMIN = 'https://t.me/xudoybergan0v'
+
+# API_URL = {
+#     '1': 'https://sheetdb.io/api/v1/6l88e9ljvile7',
+#     '2': 'https://sheetdb.io/api/v1/y2cl3rwvjwc9e'
+# }
+
+# GROUPS = {
+#     '1': -4504435403,
+#     '2': -4579819207, 
+#     '3': -4591173539
+# }
+
+# await bot.set_chat_permissions(chat_id=chat_id, can_invite_users=False)
+
